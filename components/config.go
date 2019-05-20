d52f4ffcd48bf064f53303fd70313657093a3f12
zhouyang <zhouyang@shein.com>
2019-05-20 17:42:00 +0800

    增加post

--- components/config.go
+++ components/config.go
@@ -1,13 +1,14 @@
 package components
 
 import (
+	"log"
+	url2 "net/url"
 	"path/filepath"
 	"strings"
 )
 
 const (
 	DEFAULT_THREADS    = 20     /* 默认线程数 */
-	DEFAULT_METHOD     = "GET"  /* 默认GET方法 */
 	DEFAULT_PROTOCOL   = "http" /* 默认http请求 */
 	DEFAULT_HTTP_PORT  = 80     /* http默认80端口 */
 	DEFAULT_HTTPS_PORT = 443    /* https默认443端口 */
@@ -15,15 +16,23 @@ const (
 
 type Config struct {
 	Address Remote `yaml:"api_address"`
-	Method  string `yaml:"method"`
+	Method  Method `yaml:"method"`
 	Test    Test   `yaml:"test"`
 }
 
 type Remote struct {
-	Protocol string `yaml:"protocol"`
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	Path     string `yaml:"path"`
+	Protocol string            `yaml:"protocol"`
+	Host     string            `yaml:"host"`
+	Port     int               `yaml:"port"`
+	Path     string            `yaml:"path"`
+	Query    map[string]string `yaml:"query"`
+}
+
+type Method struct {
+	Type        string                 `yaml:"type"`
+	FormBody    url2.Values            `yaml:"form_body"`
+	JsonBody    map[string]interface{} `yaml:"json_body"`
+	ContentType string                 `yaml:"content_type"`
 }
 
 type Test struct {
@@ -32,7 +41,12 @@ type Test struct {
 }
 
 var GlobalConfig Config
+var SupportMethods = []string{
+	"get",
+	"post",
+}
 
+//初始化操作
 func init() {
 	parseYml()
 	validate()
@@ -46,13 +60,15 @@ func parseYml() {
 
 //验证配置信息
 func validate() {
-	if strings.ToUpper(GlobalConfig.Method) != DEFAULT_METHOD {
-		GlobalConfig.Method = DEFAULT_METHOD
+	method := strings.ToLower(GlobalConfig.Method.Type)
+	support := false
+	for _, supportMethod := range SupportMethods {
+		if supportMethod == method {
+			support = true
+		}
 	}
-	if GlobalConfig.Address.Protocol == DEFAULT_PROTOCOL {
-		GlobalConfig.Address.Port = DEFAULT_HTTP_PORT
-	} else {
-		GlobalConfig.Address.Port = DEFAULT_HTTPS_PORT
+	if support == false {
+		log.Fatalf("不支持的请求方式：%s", method)
 	}
 	if GlobalConfig.Test.Threads <= 0 {
 		GlobalConfig.Test.Threads = DEFAULT_THREADS
@@ -67,5 +83,13 @@ func (remote *Remote) UriToString() string {
 	} else if remote.Protocol == "https" && remote.Port == DEFAULT_HTTPS_PORT {
 		port = ""
 	}
-	return remote.Protocol + "://" + remote.Host + port + remote.Path
+	query := ""
+	if len(remote.Query) > 0 {
+		query = "?"
+		for attr, value := range remote.Query {
+			query += attr + "=" + value + "&"
+		}
+		query = strings.Trim(query, "&")
+	}
+	return remote.Protocol + "://" + remote.Host + port + remote.Path + query
 }
---
+++ components/request.go
@@ -0,0 +1,87 @@
+package components
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var total int
+var url string
+var handle func() (resp *http.Response, err error)
+
+//初始化操作
+func init() {
+	switch GlobalConfig.Method.Type {
+	case "get":
+		handle = get
+	case "post":
+		handle = post
+	}
+}
+
+//开始跑喽
+func Start() {
+	url = GlobalConfig.Address.UriToString()
+	times := GlobalConfig.Test.Times / GlobalConfig.Test.Threads
+	now := time.Now().UnixNano()
+	log.Printf("%d个线程请求开始..", GlobalConfig.Test.Threads)
+
+	for i := 0; i < GlobalConfig.Test.Threads; i++ {
+		wg.Add(1)
+		go groupRequest(times)
+	}
+
+	wg.Wait()
+	end := time.Now().UnixNano()
+	log.Printf("总共请求%d次", total)
+	log.Printf("总共耗时%dns", end-now)
+}
+
+//循环发送请求
+func groupRequest(times int) {
+	for i := 0; i < times; i++ {
+		sendRequest()
+	}
+	total += times
+	defer wg.Done()
+}
+
+//发送请求
+func sendRequest() {
+	response, err := handle()
+	if err != nil {
+		log.Println("something went wrong")
+	}
+	php, _ := ioutil.ReadAll(response.Body)
+	log.Printf("%s", php)
+	//这一步必须要做，不然golang会缓存请求
+	//@see https://stackoverflow.com/questions/33238518/what-could-happen-if-i-dont-close-response-body-in-golang
+	defer response.Body.Close()
+}
+
+//普通get请求
+func get() (resp *http.Response, err error) {
+	return http.Get(url)
+}
+
+//普通post请求，需要加上body内容
+func post() (resp *http.Response, err error) {
+	contentType := GlobalConfig.Method.ContentType
+	var body io.Reader
+	/* 判断ContentType，分别读取不同的结构作为body */
+	if GlobalConfig.Method.ContentType == "application/json" {
+		jsonByte, _ := json.Marshal(GlobalConfig.Method.JsonBody)
+		body = bytes.NewBuffer(jsonByte)
+	} else {
+		body = strings.NewReader(GlobalConfig.Method.FormBody.Encode())
+	}
+	return http.Post(url, contentType, body)
+}
